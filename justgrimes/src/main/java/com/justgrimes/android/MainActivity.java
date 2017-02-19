25d348521d767cac245763e9c58130cfd1005058
Eric Mill <eric@konklone.com>
2017-02-19 22:14:19 +0200

    move from Eclipse to Android Studio

---
+++ justgrimes/src/main/java/com/justgrimes/android/MainActivity.java
@@ -0,0 +1,23 @@
+package com.justgrimes.android;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.justgrimes.android.R;
+
+
+public class MainActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		String phone = getResources().getString(R.string.phone_number);
+		startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel://" + phone)));
+		
+		finish();
+	}
+	
+}
